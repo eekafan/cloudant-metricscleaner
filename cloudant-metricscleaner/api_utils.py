1d7050f95490b3e7adc058790fef072ae70ae320
Andrew-Rombach <andrew.rombach@uk.ibm.com>
2019-06-29 12:10:59 +0100

    Remove unnecessary dependency on flask

--- cloudant-metricscleaner/api_utils.py
+++ cloudant-metricscleaner/api_utils.py
@@ -1,4 +1,4 @@
-from flask import request
+
 import json
 import time
 import datetime
@@ -291,17 +291,3 @@ def create_adminsession(url,username, password):
      error_code = 200
     return adminsession,error_response,error_code
 
-def find_requestuser(request):
- try:
-  auth = request.authorization
-  if auth:
-   return auth.username
-  else:
-   cookieauth = request.cookies.get("AuthSession")
-   if cookieauth is not None:
-    cookieauthparts = str(base64.urlsafe_b64decode(str(cookieauth))).split(':')
-    if len(cookieauthparts) > 1:
-     return cookieauthparts[0]
-  return None
- except Exception as e:
-  return None
